2ddf85174fc6faa362cfad306e5ed317ad25a85d
Volha Sakharevich <vuictorovna@gmail.com>
2023-09-14 21:39:19 +0100

    Add pdf_loader

--- reader.py
+++ reader.py
@@ -1,19 +1,9 @@
-import os
-
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyPDFDirectoryLoader
 
 
 # Function to load pdfs
 def pdf_doc_loader(pdfs):
-    loader = PyPDFLoader(pdfs)
-    docs = loader.load()
-    return docs
+   loader = PyPDFDirectoryLoader(pdfs)
+   docs = loader.load()
+   return docs
 
-
-pdf_directory = (
-    "C:\\Users\\VolhaSakharevich\\Documents\\Dev\\climate-change-action-assistant\\pdfs"
-)
-
-for filename in os.listdir(pdf_directory):
-    if filename.endswith(".pdf"):
-        pdf_file_path = os.path.join(pdf_directory, filename)
